25ec4de4fb8674374f7fc65d3df021c9c43ad625
Paul Reinholdtsen <paul@bitsy.services>
2026-03-17 16:08:11 -0500

    pragma solidity ^0.8.30; IUintToAddressCloner

--- IUintToAddressCloner.sol
+++ IUintToAddressCloner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.30;
 
 /// @notice Deploy clones of a contract that initializes via an array mapping uint to address.
 /// @author Paul Reinholdtsen (reinholdtsen.eth)
