0d49d5c3775451b0428a99680b30cee73aa39c1d
Paul Reinholdtsen <paul@bitsy.services>
2026-03-17 18:03:09 -0500

    Improve IUintToAddressCloner

--- IUintToAddressCloner.sol
+++ IUintToAddressCloner.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-/// @notice Deploy clones of a contract that initializes via an array mapping uint to address.
-/// @author Paul Reinholdtsen (reinholdtsen.eth)
+/// @notice Deploy contracts that initializes via an array mapping uint to address.
 interface IUintToAddressCloner {
     /// @notice Map a chainId to a local address.
     struct KeyValue {
@@ -12,21 +11,21 @@ interface IUintToAddressCloner {
 
     /// @notice Predict the address of a clone.
     /// @param keyValues The array of key value pairs sorted by key.
-    /// @return clone The predicted address of the clone.
+    /// @return exists Whether the clone has already been deployed.
+    /// @return home The predicted address of the clone.
     /// @return salt The salt used to create the clone. salt = keccak256(abi.encode(keyValues));
-    function cloneAddress(KeyValue[] memory keyValues) external view returns (address clone, bytes32 salt);
+    function made(KeyValue[] memory keyValues) external view returns (bool exists, address home, bytes32 salt);
 
     /// @notice Create a clone if it doesn't already exist.
     /// @param keyValues The array of key value pairs sorted by key.
-    /// @return clone The address of the new or existing clone.
-    /// @return salt The salt used to create the clone. salt = keccak256(abi.encode(keyValues));
-    function clone(KeyValue[] memory keyValues) external returns (address clone, bytes32 salt);
-
-    /// @notice Revert if someone tries to reinitialize an instance.
-    error InitializedAlready();
+    /// @return home The address of the new or existing clone.
+    function make(KeyValue[] memory keyValues) external returns (address home);
 
     /// @notice Emit when a clone is created.
-    /// @return clone The address of the new clone.
+    /// @return home The address of the new clone.
     /// @return salt The salt used to create the clone. salt = keccak256(abi.encode(keyValues));
-    event Cloned(address indexed clone, bytes32 indexed salt);
+    event Made(address indexed home, bytes32 indexed salt);
+
+    /// @notice Revert if someone tries to reinitialize an instance.
+    error MadeAlready();
 }
