d5fa1b0569d074021c9c5d9faaba5c299907facc
Paul Reinholdtsen <paul@bitsy.services>
2026-03-18 16:56:49 -0500

    Squash merge setup to main

---
+++ IAddressLookup.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @notice Map a single predictable contract address to a chain‑specific address.
+/// @dev This deterministic lookup pattern enables the creation of immutable contracts
+/// at deterministic addresses even if they depend on initialization data that varies by chain.
+interface IAddressLookup {
+    /// @return local address for the current chain.
+    function value() external view returns (address local);
+}
---
+++ IUintToUintMaker.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @notice Make a lookup mapping uint256 to uint256.
+interface IUintToUintMaker {
+    /// @notice Map a key to a value.
+    struct KeyValue {
+        uint256 key;
+        uint256 value;
+    }
+
+    /// @notice Predict the address of a lookup.
+    /// @param keyValues The array of key value pairs sorted by key.
+    /// @return exists Whether the lookup already exists.
+    /// @return home The predicted address of the lookup.
+    /// @return salt The salt used to create the lookup. salt = keccak256(abi.encode(keyValues));
+    function made(KeyValue[] memory keyValues) external view returns (bool exists, address home, bytes32 salt);
+
+    /// @notice Create a lookup if it doesn't already exist.
+    /// @param keyValues The array of key value pairs sorted by key.
+    /// @return home The address of the new or existing lookup.
+    function make(KeyValue[] memory keyValues) external returns (address home);
+
+    /// @notice Emit when a lookup is made.
+    event Made(address indexed home, bytes32 indexed salt);
+
+    /// @notice Revert if someone tries to reinitialize an instance.
+    error Unauthorized();
+}
