138a9d82532e74871839aa0171373428f320388f
Paul Reinholdtsen <paul@bitsy.services>
2026-05-03 15:48:14 -0500

    add IStringLookup and IUintToStringMaker

---
+++ IUintToStringMaker.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {IMaker} from "./IMaker.sol";
+
+/**
+ *  @notice Make a lookup mapping uint256 to string.
+ */
+interface IUintToStringMaker is IMaker {
+    /**
+     * @notice Map a key to a value.
+     */
+    struct KeyValue {
+        uint256 key;
+        string value;
+    }
+
+    /**
+     * @notice Predict the address of a lookup.
+     * @param keyValues The array of key value pairs sorted by key.
+     * @param variant Discriminator allowing distinct addresses for the same keyValues.
+     * @return exists Whether the lookup already exists.
+     * @return home The predicted address of the lookup.
+     * @return salt The salt used to create the lookup. salt = keccak256(abi.encode(keyValues, variant));
+     */
+    function made(KeyValue[] memory keyValues, uint256 variant)
+        external
+        view
+        returns (bool exists, address home, bytes32 salt);
+
+    /**
+     * @notice Create a lookup if it doesn't already exist.
+     * @param keyValues The array of key value pairs sorted by key.
+     * @param variant Discriminator allowing distinct addresses for the same keyValues.
+     * @return home The address of the new or existing lookup.
+     */
+    function make(KeyValue[] memory keyValues, uint256 variant) external returns (address home);
+}
